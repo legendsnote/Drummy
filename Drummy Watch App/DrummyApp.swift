85a75742b972a619bf07989232682f7d8375943c
Geraldo <geraldo.p.lutan@gmail.com>
2024-05-14 17:41:04 +0700

    Initial Commit

---
+++ Drummy Watch App/DrummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrummyApp.swift
+//  Drummy Watch App
+//
+//  Created by Geraldo Pannanda Lutan on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Drummy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
